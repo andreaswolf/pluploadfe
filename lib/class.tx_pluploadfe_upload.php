6fe6fa1ecf02c2129ac086c24d11e9c85a480206
Andreas Wolf <dev@a-w.io>
2016-03-22 16:14:48 +0100

    [BUGFIX] Set application/json content type

--- lib/class.tx_pluploadfe_upload.php
+++ lib/class.tx_pluploadfe_upload.php
@@ -294,6 +294,7 @@ class tx_pluploadfe_upload {
 		header('Cache-Control: no-store, no-cache, must-revalidate');
 		header('Cache-Control: post-check=0, pre-check=0', FALSE);
 		header('Pragma: no-cache');
+		header('Content-Type: application/json');
 	}
 
 	/**
